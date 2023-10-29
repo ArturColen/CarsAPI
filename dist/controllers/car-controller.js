59a1f60fe0fcd965530e1bc06acef92ccadc8867
LucasMeloSena <lucasmelosena@gmail.com>
2023-10-29 17:28:18 -0300

    updates

--- dist/controllers/car-controller.js
+++ dist/controllers/car-controller.js
@@ -65,27 +65,60 @@ const updateCarController = (req, res) => __awaiter(void 0, void 0, void 0, func
         const db = conn.db();
         const carCollection = db.collection("car");
         const id = req.params.id;
-        if (id.toString().trim() == "" || id == null || id == undefined || typeof id != 'string') {
+        const data = {
+            nome: req.body.nome,
+            preco: req.body.preco,
+            fabricante: req.body.fabricante,
+            categoria: req.body.categoria,
+            ano_lancamento: req.body.ano_lancamento,
+            assentos: req.body.assentos,
+            potencia: req.body.potencia,
+            aro: req.body.aro,
+            versao: req.body.versao,
+            peso: req.body.peso,
+            abastecimento: req.body.abastecimento
+        };
+        if (id.toString().trim() === "" || id === null || id === undefined || typeof id !== 'string') {
             throw new Error("Id do automóvel inválido!");
         }
+        else if (data.nome.toString().trim() === "" || data.nome === null || data.nome === undefined || typeof data.nome !== 'string') {
+            throw new Error("Nome do automóvel inválido!");
+        }
+        else if (data.preco.toString().trim() === "" || data.preco === null || data.preco === undefined || typeof data.preco !== 'number') {
+            throw new Error("Preço do automóvel inválido!");
+        }
+        else if (data.fabricante.toString().trim() === "" || data.fabricante === null || data.fabricante === undefined || typeof data.fabricante !== 'string') {
+            throw new Error("Fabricante do automóvel inválido!");
+        }
+        else if (data.categoria.toString().trim() === "" || data.categoria === null || data.categoria === undefined || typeof data.categoria !== 'string') {
+            throw new Error("Categoria do automóvel inválido!");
+        }
+        else if (data.ano_lancamento.toString().trim() === "" || data.ano_lancamento === null || data.ano_lancamento === undefined || typeof data.ano_lancamento !== 'number') {
+            throw new Error("Ano de lançamento do automóvel inválido!");
+        }
+        else if (data.assentos.toString().trim() === "" || data.assentos === null || data.assentos === undefined || typeof data.assentos !== 'number') {
+            throw new Error("Assentos do automóvel inválido!");
+        }
+        else if (data.potencia.toString().trim() === "" || data.potencia === null || data.potencia === undefined || typeof data.potencia !== 'number') {
+            throw new Error("Potência do automóvel inválido!");
+        }
+        else if (data.aro.toString().trim() === "" || data.aro === null || data.aro === undefined || typeof data.aro !== 'number') {
+            throw new Error("Aro do automóvel inválido!");
+        }
+        else if (data.versao.toString().trim() === "" || data.versao === null || data.versao === undefined || typeof data.versao !== 'string') {
+            throw new Error("Versão do automóvel inválido!");
+        }
+        else if (data.peso.toString().trim() === "" || data.peso === null || data.peso === undefined || typeof data.peso !== 'number') {
+            throw new Error("Peso do automóvel inválido!");
+        }
+        else if (data.abastecimento.toString().trim() === "" || data.abastecimento === null || data.abastecimento === undefined || typeof data.abastecimento !== 'string') {
+            throw new Error("Abastecimento do automóvel inválido!");
+        }
         const objId = new mongodb_1.ObjectId(id);
-        const update = yield carCollection.updateOne({
+        yield carCollection.updateOne({
             _id: objId
         }, {
-            $set: {
-                nome: "City",
-                preco: 100000,
-                cor: "Prata",
-                fabricante: "Honda",
-                categoria: "Sedan",
-                ano_lancamento: 2023,
-                assentos: 5,
-                potencia: 125,
-                aro: 13,
-                versao: "EXL",
-                peso: 120,
-                abastecimento: "flex"
-            }
+            $set: data
         });
         const result = yield carCollection.findOne({ _id: objId });
         res.status(200).json({ message: result });
--- src/middlewares/cors-middlewares.ts
+++ src/middlewares/cors-middlewares.ts
@@ -5,7 +5,6 @@ export const configureCORS = (app: express.Express) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        
         next();
     });
 };
