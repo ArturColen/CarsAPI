63dc1fe51f528f96808e3c31cd9bac97bfded849
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-11-09 01:08:47 -0300

    feat: addition of a script to validate that the JSON format is valid

--- dist/server.js
+++ dist/server.js
@@ -4,12 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors_middlewares_js_1 = require("./middlewares/cors-middlewares.js");
+const cors_middleware_js_1 = require("./middlewares/cors-middleware.js");
 const cars_route_js_1 = __importDefault(require("./routes/cars-route.js"));
+const error_middleware_js_1 = require("./middlewares/error-middleware.js");
 const port = 3000;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-(0, cors_middlewares_js_1.configureCORS)(app);
+(0, cors_middleware_js_1.configureCORS)(app);
+app.use(error_middleware_js_1.errorMiddleware);
 app.use('/cars', cars_route_js_1.default);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
